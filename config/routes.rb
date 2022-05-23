0a3de8c4dc9ea98e72fa1d8a91b804ddfac1ef99
Fernando <fernandoa.rodriguezh@gmail.com>
2022-05-23 16:08:19 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  resources :videos
+  get 'courses/', to: 'courses#index'
+  get 'courses/new'
+  get 'courses/:id', to: 'courses#show', as: 'show_course'
+  get 'courses/:id/edit', to: 'courses#edit', as: 'edit_course'
+
+  post 'courses', to: 'courses#create'
+  patch 'courses/:id/', to: 'courses#update', as: 'course'
+  #post 'courses/:id/', to: 'courses#destroy', as: 'delete_course'
+  delete 'courses/:id', to: 'courses#destroy', as: 'delete_course'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
